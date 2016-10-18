928e311ed948bbe3c34d0908cf4ee0c1033d26a7
fleaf5 <dongzli@hotmail.com>
2016-10-17 22:23:24 -0400

    10.17 phase map is quite noisy, no any sign of magnetic flipping currently seen in 1e-4, 1e-3, 1e-2 scales

--- code/phase.py
+++ code/phase.py
@@ -10,8 +10,8 @@ def phase0(name = 'gb057_1.input_baseline257_freq_00_pol_LL.rebint', save = 'no'
 	phase = np.angle(conj)
 #apply mask
 	mask_indices = np.log10(np.absolute(conj)**2.)< mask_lowI
-	print 'number of unmasked points',len(phase)*len(phase[0])-np.count_nonzero(mask_indices)
-	phase[mask_indices] = 100.0
+	print 'number of unmasked points',np.count_nonzero(np.logical_not(mask_indices))
+	phase[mask_indices] = 0.0
 
 	if save !='no':
 		phase.astype('float32').T.tofile(saving_path+'phase_mask'+str(mask_lowI)+'_'+name)
@@ -20,9 +20,7 @@ def phase0(name = 'gb057_1.input_baseline257_freq_00_pol_LL.rebint', save = 'no'
 	return phase
 
 def readphase0(name = 'gb057_1.input_baseline257_freq_00_pol_LL.rebint',phase_type='phase',mask_lowI = 0.0):
-	mask=''
-	if mask_lowI != 0:
-		mask = 'mask'+str(mask_lowI)+'_'
+	mask = 'mask'+str(mask_lowI)+'_'
 
 	if phase_type == 'phase':
 		filename = saving_path + 'phase_'+mask + name
@@ -35,17 +33,33 @@ def readphase0(name = 'gb057_1.input_baseline257_freq_00_pol_LL.rebint',phase_ty
 
 def phase0_LminusR (phasel,phaser,save='no',name = 'gb057_1.input_baseline257_freq_00_pol_LL.rebint',mask_lowI=0.0):
 	phase_diff = phasel - phaser
+
+#to make sure delta psi is in the range of -pi to pi
+	renorm = phase_diff > np.pi
+	phase_diff[renorm]-=2.0*np.pi
+	renorm = phase_diff < -np.pi
+	phase_diff[renorm]+=2.0*np.pi
+
 	if save != 'no':
-		phase_diff.astype('float32').T.tofile(saving_path+'phase_diff_'+name)
+		phase_diff.astype('float32').T.tofile(saving_path+'phase_diff_mask'+str(mask_lowI)+'_'+name)
 		print 'save phase LL-RR to: '
 		print (saving_path+'phase_diff_mask'+str(mask_lowI)+'_'+name)
 	return phase_diff
 
-def plot_phase (phase,pdf,name =  'gb057_1.input_baseline257_freq_00_pol_LL.rebint' ):
+def plot_phase (phase,pdf,name =  'gb057_1.input_baseline257_freq_00_pol_LL.rebint',vrange = np.pi ):
+	if vrange == 0:
+		print 'vrange = 0, auto set vrange = pi'
+		vrange = np.pi
 #	import math
 #	phase*=180.0*3600.0/math.pi #transform into arcsec
+
+# mask data points with abnormal large delta psi
+	mask_indices = abs(phase) > vrange
+	phase[mask_indices] = 0.0
+#	phase[np.logical_not(mask_indices)] = -100	
+
 	plt.figure(1)
-	plt.imshow(phase, origin='lower',interpolation='none',aspect='auto',cmap='gray',vmin = -3.14, vmax=3.14)
+	plt.imshow(phase, origin='lower',interpolation='none',aspect='auto',cmap='seismic',vmin=-vrange,vmax=vrange)
 	plt.xlim(300,1000)
 	plt.ylim(5000,12000)
 	plt.title(name)
--- code/run.py
+++ code/run.py
@@ -1,6 +1,8 @@
 task = 4
-plotphase=1
-mask_lowI = 8.0
+plotphase=0
+mask_lowI = 6.5
+vrange = 1e-3
+filenumber = 3
 
 figure_path='/home/dzli/ism/figures/'
 # 0 plot secondary specs from dy_spec 
@@ -23,12 +25,22 @@ import matplotlib.backends.backend_pdf as pgs
 import numpy as np
 
 os.chdir('/mnt/scratch-lustre/simard/B0834_2012/Gate0/')
-namers = glob.glob('*pol_RR.rebint')
-namels = glob.glob('*pol_LL.rebint')
+[namer] = glob.glob('*freq_00_pol_RR.rebint')
+[namel] = glob.glob('*freq_00_pol_LL.rebint')
 #for task 0,1,2: names = namers
 if task == 0:
 	pdf = pgs.PdfPages(figure_path+'sec_pol_RR.pdf')
-	for name in names:
+	for i in xrange(0,filenumber):
+		name = namer.replace('freq_00','freq_0'+str(i))
+		data = sp.readdy(name=name)
+	#sp.plot_spec(data,save='y')
+		ic = sp.conj_spec(data)
+		sp.plot_spec(ic,pdf,spec_type='sec',name=name)
+	pdf.close()
+
+	pdf = pgs.PdfPages(figure_path+'sec_pol_LL.pdf')
+	for i in xrange(0,filenumber):
+		name = namel.replace('freq_00','freq_0'+str(i))
 		data = sp.readdy(name=name)
 	#sp.plot_spec(data,save='y')
 		ic = sp.conj_spec(data)
@@ -36,14 +48,28 @@ if task == 0:
 	pdf.close()
 
 elif task == 1:
-	for name in names:
+	for i in xrange(0,filenumber):
+		name = namer.replace('freq_00','freq_0'+str(i))
+		data = sp.readdy(name = name)
+		sp.conj_spec(data,save = 'y',name = name)
+
+	for i in xrange(0,filenumber):
+		name = namer.replace('freq_00','freq_0'+str(i))
 		data = sp.readdy(name = name)
 		sp.conj_spec(data,save = 'y',name = name)
 	task=2
 
 if task == 2:
 	pdf = pgs.PdfPages(figure_path+'sec_pol_RR.pdf')
-	for name in names:
+	for i in xrange(0,filenumber):
+		name = namel.replace('freq_00','freq_0'+str(i))
+		ic = sp.readconj(name=name)
+		sp.plot_spec(ic,pdf,spec_type='sec',name=name)
+	pdf.close()
+
+	pdf = pgs.PdfPages(figure_path+'sec_pol_LL.pdf')
+	for i in xrange(0,filenumber):
+		name = namel.replace('freq_00','freq_0'+str(i))
 		ic = sp.readconj(name=name)
 		sp.plot_spec(ic,pdf,spec_type='sec',name=name)
 	pdf.close()
@@ -51,45 +77,59 @@ if task == 2:
 if task == 3:
 	import check as ck
 	pdf = pgs.PdfPages(figure_path+'sec_LLminusRR.pdf')
-	for i in xrange(0,len(namels)):
-		ck.check_lr(pdf,spec_type='sec',namel=namels[i],namer=namers[i])
+	for i in xrange(0,filenumber):
+		namell = namel.replace('freq_00','freq_0'+str(i))
+		namerr = namer.replace('freq_00','freq_0'+str(i))
+		ck.check_lr(pdf,spec_type='sec',namel=namell,namer=namerr)
 	pdf.close()
 
 elif task == 4:
 	import phase as psi 
-	for i in xrange(0,len(namels)):
-		phasel = psi.phase0(name = namels[i],save ='y',mask_lowI = mask_lowI)
-		phaser = psi.phase0(name = namers[i],save ='y',mask_lowI = mask_lowI)
-		phase_diff = psi.phase0_LminusR(phasel,phaser,save='y',name = namels[i],mask_lowI=mask_lowI)
+	for i in xrange(0,filenumber):
+		namell = namel.replace('freq_00','freq_0'+str(i))
+		namerr = namer.replace('freq_00','freq_0'+str(i))
+		phasel = psi.phase0(name = namell,save ='y',mask_lowI = mask_lowI)
+		phaser = psi.phase0(name = namerr,save ='y',mask_lowI = mask_lowI)
+		phase_diff = psi.phase0_LminusR(phasel,phaser,save='y',name = namell,mask_lowI=mask_lowI)
 		del phasel,phaser,phase_diff
 	task = 5
 
 if task == 5:
 	import phase as psi
 
+	mask = ''
 	if mask_lowI !=0:
-		mask = '_mask'+str(mask_lowI)+'_'
+		mask = '_mask'+str(mask_lowI)
+	if vrange != 0:
+		mask = mask + '_vrange'+str(vrange)
 #plot phase LL minus RR
-	pdf = pgs.PdfPages(figure_path+'phase_LLminusRR'+mask+'.pdf')
-	for i in xrange(0,len(namels)):
-		phase = psi.readphase0(name = namels[i],phase_type = 'diff',mask_lowI=mask_lowI)
-		psi.plot_phase(phase,pdf,name=namels[i])
+	pdf = pgs.PdfPages(figure_path+'phase_LLminusRR'+mask+'_'+str(filenumber)+'.pdf')
+	for i in xrange(0,filenumber):
+	#	namell = namel.replace('freq_00','freq_0'+str(i))
+		namell = namel
+		vrange *=10
+		phase = psi.readphase0(name = namell,phase_type = 'diff',mask_lowI=mask_lowI)
+		name = namel.replace('freq_00_pol_LL','freq_0'+str(i)+'LLminusRR')
+		name = namel.replace('pol_LL','LLminusRR')
+		psi.plot_phase(phase,pdf,name=name,vrange = vrange)
 		del phase
 	pdf.close()
 
 	if plotphase == 1:
 	#plot phase LL and phase RR
-		pdf = pgs.PdfPages(figure_path+'phase_LL'+mask+'.pdf')
-		for i in xrange(0,len(namels)):
-			phase = psi.readphase0(name = namels[i],mask_lowI=mask_lowI)
-			psi.plot_phase(phase,pdf,name=namels[i])
+		pdf = pgs.PdfPages(figure_path+'phase_LL'+mask+'_'+str(filenumber)+'.pdf')
+		for i in xrange(0,filenumber):
+			namell = namel.replace('freq_00','freq_0'+str(i))
+			phase = psi.readphase0(name = namell,mask_lowI=mask_lowI)
+			psi.plot_phase(phase,pdf,name=namell,vrange=vrange)
 			del phase
 		pdf.close()
 		
-		pdf = pgs.PdfPages(figure_path+'phase_RR'+mask+'.pdf')
-		for i in xrange(0,len(namels)):
-			phase = psi.readphase0(name = namers[i],mask_lowI=mask_lowI)
-			psi.plot_phase(phase,pdf,name=namers[i])
+		pdf = pgs.PdfPages(figure_path+'phase_RR'+mask+'_'+str(filenumber)+'.pdf')
+		for i in xrange(0,filenumber):
+			namerr = namer.replace('freq_00','freq_0'+str(i))
+			phase = psi.readphase0(name = namerr,mask_lowI=mask_lowI)
+			psi.plot_phase(phase,pdf,name=namerr,vrange = vrange)
 			del phase
 		pdf.close()
 
